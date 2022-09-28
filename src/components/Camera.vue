a9adcd2e8d0dce41d0df33c0ec77f61e1f835679
Adam Nord <adam.nord@abbgymnasiet.se>
2022-09-28 21:22:03 +0200

    fixed formatting

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,30 +1,30 @@
 <script setup>
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from "vue";
 
-const video = ref(null)
+const video = ref(null);
 
 async function openCamera() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
-          facingMode: 'environment',
-          height: window.screen.availWidth,
-          aspectRatio: { exact: (16/9) }
+        facingMode: "environment",
+        height: window.screen.availWidth,
+        aspectRatio: { exact: 16 / 9 },
       },
       audio: false,
     });
     const [track] = stream.getVideoTracks();
-      const capabilities = track.getCapabilities()
-      const settings = track.getSettings()
-    video.value.srcObject = stream
+    const capabilities = track.getCapabilities();
+    const settings = track.getSettings();
+    video.value.srcObject = stream;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
 onMounted(async () => {
-   await openCamera()
-})
+  await openCamera();
+});
 </script>
 
 <template>
