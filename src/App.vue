28a1bde50b0477e266c2728fa972fc9536067796
Adam Nord <adam.nord@abbgymnasiet.se>
2022-09-28 19:27:36 +0200

    removed dark mode from button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 const count = ref(0)
 function increment() {
-  count.value++
+    count.value++
 }
 </script>
 
@@ -10,5 +10,7 @@ function increment() {
     <div class="flex flex-col p-4 w-full max-w-3xl">
         <h1 class="text-3xl font-bold">Leafify</h1>
         <p>Count: {{count}}</p>
-        <button @click="increment" type="button" class="w-fit py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Increment</button>    </div>
+        <button @click="increment" type="button"
+            class="w-fit py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Increment</button>
+    </div>
 </template>
